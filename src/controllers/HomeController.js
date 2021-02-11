08d3d3af7de0cef85d30df29faaa2d4c967a330c
Isaac Moura <isaacestudo1999@gmail.com>
2021-02-11 15:33:15 -0300

    ...

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,9 +1,17 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res){
-    res.json({
-      tudoCerto: true,
-    })
-  }
-}
+  async index(req, res){
+    const novoAluno = await Aluno.create({
+      nome: 'Maria',
+      sobrenome: 'Miranda',
+      email: 'maria@gmail.com',
+      idade: 57,
+      peso: 60,
+      altura: 1.60,
+    });
+    res.json(novoAluno);
+  };
+};
 
 export default new HomeController();
